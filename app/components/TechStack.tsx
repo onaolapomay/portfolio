e845f5968aa29dc46c055151b845b0f74dac15ee
onaolapomay <raphy.mayo@gmail.com>
2026-06-14 00:36:55 +0100

    changed and increased the text and dark color

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -31,7 +31,7 @@ export default function TechStack () {
                         key={tech.name}
                         className='flex items-center gap-3 rounded-xl border border-zinc-300 dark:border-zinc-800 bg-zinc-100 dark:bg-zinc-900 p-4 transition hover:border-zinc-700'>
                             <Icon size={24} className='text-zinc-900 dark:text-zinc-100'/>
-                            <span className='text-zinc-300 dark:text-white'>{tech.name}</span>
+                            <span className='text-zinc-800 dark:text-zinc-100'>{tech.name}</span>
 
                         </div>
                     )
